731161d12f8605a4eb362cc754fbb308ed29d61c
jesuslvco <jesuslvco@gmail.com>
2018-10-24 08:43:06 -0500

    Update www/js/config/connections.js
    rutas de conexion

--- www/js/config/connections.js
+++ www/js/config/connections.js
@@ -7,7 +7,7 @@ define([], function(){
         domain:'http://directoriocomercialdelcentro.com/wp-json/wp/v2/',
 	    search:{
             //url:'search/%s?fields=id,title.rendered,link,type,excerpt,content,featured_image',
-            url:'posts?fields=id,title.rendered,link,type,excerpt,content,featured_image,categories&page=%page&per_page=%per_page&search=%s',
+            url:'posts?fields=id,title.rendered,link,type,excerpt,content,featured_image,categories&page=%page&per_page=%per_page&search=%s', 
             //url:'posts?search=comida&fields=id,title.rendered,link,type,excerpt,content,featured_image,categories&page=1&per_page=5&categories=41,42&&orderby=relevance',
             type:'GET',
             dataType:'json'
