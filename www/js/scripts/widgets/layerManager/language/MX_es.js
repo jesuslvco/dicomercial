5900d91597bd449e2714834c07d94ff098074925
jesuslvco <jesuslvco@gmail.com>
2018-10-24 16:31:14 -0500

    base ejemplo externo - borrar en futuro -

---
+++ www/js/scripts/widgets/layerManager/language/MX_es.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+var layerManager_lang = {
+	NOT_LANGUAGE:'Error idioma',
+	SEARCH:'Buscar',
+	RESULTS:'Resultados',
+	OTHER_RESULTS:'Otros resultados'
+}
+//@ sourceURL=LAYERMANAGER_LANGUAGE.js
