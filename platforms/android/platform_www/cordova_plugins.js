56f87a854d9ae33b773168e7f2e4d0a30a0e635b
jesuslvco <jesuslvco@gmail.com>
2018-12-13 15:27:17 -0600

    Se eliminó el plugnin de compartir socialmente

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -6,18 +6,6 @@ module.exports = [
         "clobbers": [
             "call"
         ]
-    },
-    {
-        "file": "plugins/es6-promise-plugin/www/promise.js",
-        "id": "es6-promise-plugin.Promise",
-        "runs": true
-    },
-    {
-        "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
-        "id": "cordova-plugin-x-socialsharing.SocialSharing",
-        "clobbers": [
-            "window.plugins.socialsharing"
-        ]
     }
 ];
 module.exports.metadata = 
--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -1,20 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/es6-promise-plugin/www/promise.js",
-        "id": "es6-promise-plugin.Promise",
-        "pluginId": "es6-promise-plugin",
-        "runs": true
-    },
-    {
-        "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
-        "id": "cordova-plugin-x-socialsharing.SocialSharing",
-        "pluginId": "cordova-plugin-x-socialsharing",
-        "clobbers": [
-            "window.plugins.socialsharing"
-        ]
-    },
-    {
         "file": "plugins/cordova-plugin-call-number/www/CallNumber.js",
         "id": "cordova-plugin-call-number.CallNumber",
         "pluginId": "cordova-plugin-call-number",
@@ -26,8 +12,6 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "es6-promise-plugin": "4.2.2",
-    "cordova-plugin-x-socialsharing": "5.4.1",
     "cordova-plugin-call-number": "1.0.1",
     "cordova-plugin-whitelist": "1.3.3"
 }
